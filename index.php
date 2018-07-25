551fcda7c47072c33c9497086077f2cc339717e0
santosaputro <psysaputro@gmail.com>
2018-07-26 05:00:03 +0700

    stage2

--- index.php
+++ index.php
@@ -7,14 +7,18 @@
     <title></title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://unpkg.com/ionicons@4.2.5/dist/css/ionicons.min.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Playfair+Display:700,700italic,400,400italic,900,900italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
 
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   </head>
   <body>
 
@@ -31,9 +35,9 @@
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="nav-brand"><a href="#">Your <br><span>Brand</span></a></div>
         <div class="collapse navbar-collapse" id="navbar">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
             <li><a href="#"></a></li>
 
           </ul>
@@ -50,21 +54,48 @@
     </nav>
 
     <div class="pd-3"></div>
-
-    <!-- <div class="container-fluid">
-      <div style="background: red; height: 5em;">
-
-      </div>
-    </div> -->
+    <!-- <br><br><br><br><br> -->
 
     <section>
-      <div class="header-image-card">
+      <div class="image-card-left">
         <div class="container-fluid">
           <div class="row">
             <img class="img-responsive" src="assets/images/image2.jpg" alt="image2">
-            <div class="header-image-overlay">
+            <div class="image-overlay-left">
               <h1>Lorem ipsum <br>dolor sit amet, consectetur adipisicing elit</h1> <br>
-              <!-- <a href="#" class="btn btn-default">More Detail</a> -->
+              <a href="#" class="link-go dark">More Detail <i class="animated infinite slow shake icon ion-ios-remove"></i><i class="animated infinite slow shake icon ion-ios-arrow-round-forward"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <h2>Lorem Ipsum Dolor Sit Amet</h2>
+            <blockquote cite="http://">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <br><br><br><br>
+              <a href="#" class="link-go dark">More Detail <i class="icon ion-ios-remove"></i><i class="icon ion-ios-arrow-round-forward"></i></a>
+            </blockquote>
+          </div>
+          <div class="text-overlay">
+            <div class="text-overlay-right">
+              <div class="col-md-7">
+                <h2>Lorem Ipsum Dolor Sit Amet</h2>
+                <blockquote class="bc-none">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut.
+                  <br><br><br>
+                  <a href="#" class="link-go dark">More Detail <i class="icon ion-ios-remove"></i><i class="icon ion-ios-arrow-round-forward"></i></a>
+                </blockquote>
+              </div>
             </div>
           </div>
         </div>
@@ -72,7 +103,30 @@
     </section>
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <section>
+      <div class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              &copy; 2018 Santololl All Right Reserved
+            </div>
+            <div class="col-md-4 text-center">
+              <i class="icon ion-md-code"></i> with <i class="icon ion-ios-heart"></i> by Santololl
+            </div>
+            <div class="col-md-4">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <script type="text/javascript">
+
+    </script>
+
+    <!-- <script src="https://unpkg.com/ionicons@4.2.5/dist/ionicons.js"></script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
