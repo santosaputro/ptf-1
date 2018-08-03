500ae69b8d465c7b5c35709e0c837a8f6b14ff2c
santosaputro <psysaputro@gmail.com>
2018-08-03 14:55:34 +0700

    finished before mobile view

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
     <![endif]-->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js"></script>
   </head>
   <body>
 
@@ -57,13 +59,13 @@
     <!-- <br><br><br><br><br> -->
 
     <section>
-      <div class="image-card-left">
+      <div class="lg-image-card-left">
         <div class="container-fluid">
           <div class="row">
             <img class="img-responsive" src="assets/images/image2.jpg" alt="image2">
-            <div class="image-overlay-left">
+            <div class="lg-image-overlay-left">
               <h1>Lorem ipsum <br>dolor sit amet, consectetur adipisicing elit</h1> <br>
-              <a href="#" class="link-go dark">More Detail <i class="animated infinite slow shake icon ion-ios-remove"></i><i class="animated infinite slow shake icon ion-ios-arrow-round-forward"></i></a>
+              <a href="#" class="link-go c-dark">More Detail <i class="animated infinite slow shake icon ion-ios-remove"></i><i class="animated infinite slow shake icon ion-ios-arrow-round-forward"></i></a>
             </div>
           </div>
         </div>
@@ -82,7 +84,7 @@
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               <br><br><br><br>
-              <a href="#" class="link-go dark">More Detail <i class="icon ion-ios-remove"></i><i class="icon ion-ios-arrow-round-forward"></i></a>
+              <a href="#" class="link-go c-dark">More Detail <i class="icon ion-ios-remove"></i><i class="icon ion-ios-arrow-round-forward"></i></a>
             </blockquote>
           </div>
           <div class="text-overlay">
@@ -93,7 +95,7 @@
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut.
                   <br><br><br>
-                  <a href="#" class="link-go dark">More Detail <i class="icon ion-ios-remove"></i><i class="icon ion-ios-arrow-round-forward"></i></a>
+                  <a href="#" class="link-go c-dark">More Detail <i class="icon ion-ios-remove"></i><i class="icon ion-ios-arrow-round-forward"></i></a>
                 </blockquote>
               </div>
             </div>
@@ -102,7 +104,158 @@
       </div>
     </section>
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <section>
+      <div class="section3-overlay">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 no-pd">
+              <div class="img-card">
+                <img src="assets/images/image1.jpg" alt="image1" class="img-responsive">
+                <div class="img-card-caption">
+                  <div class="img-card-title">
+                    ARTICLE
+                  </div>
+                  <div class="img-card-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  </div>
+                  <div class="img-card-footer">
+                    25 . 07 . 2018
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 no-pd">
+              <div class="img-card">
+                <img src="assets/images/image4.jpg" alt="image4" class="img-responsive">
+                <div class="img-card-caption">
+                  <div class="img-card-title">
+                    ARTICLE
+                  </div>
+                  <div class="img-card-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  </div>
+                  <div class="img-card-footer">
+                    25 . 07 . 2018
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 no-pd">
+              <div class="img-card">
+                <img src="assets/images/image5.jpg" alt="image5" class="img-responsive">
+                <div class="img-card-caption bg-dark c-white">
+                  <div class="img-card-title">
+                    ARTICLE
+                  </div>
+                  <div class="img-card-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                  </div>
+                  <div class="img-card-footer">
+                    25 . 07 . 2018
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="shadow">
+          <div class="row">
+            <div class="col-md-6 no-pd">
+              <img src="assets/images/image1.jpg" alt="image1" class="img-responsive">
+            </div>
+            <div class="col-md-6 no-pd">
+              <div class="col-md-12 no-pd">
+                <div class="social-card bg-twitter c-white">
+                  <div class="social-header">
+                    <i class="icon ion-logo-twitter"></i>
+                    <span>@santololl</span>
+                  </div>
+                  <div class="wide-social-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 no-pd">
+                <img src="assets/images/image4.jpg" alt="image4" class="img-responsive">
+              </div>
+              <div class="col-md-6 no-pd">
+                <img src="assets/images/image5.jpg" alt="image5" class="img-responsive">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3 no-pd">
+              <img src="assets/images/image4.jpg" alt="image4" class="img-responsive">
+            </div>
+            <div class="col-md-3 no-pd">
+              <div class="social-card bg-gplus c-white">
+                <div class="social-header">
+                  <i class="icon ion-logo-googleplus"></i>
+                  <span>@santololl</span>
+                </div>
+                <div class="md-social-content">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 no-pd">
+              <img src="assets/images/image2.jpg" alt="image2" class="img-responsive">
+            </div>
+            <div class="col-md-3 no-pd">
+              <img src="assets/images/image1.jpg" alt="image1" class="img-responsive">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section>
+      <div class="section5-overlay" style="z-index: -1; position: relative">
+        <div class="pd-3"></div>
+        <div class="pd-3"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-2 no-pd">
+              <h3>Lorem Ipsum</h3>
+              <!-- <div style="margin-right: 6em;"> -->
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam.
+              <!-- </div> -->
+            </div>
+            <div class="col-md-4 col-md-offset-2 no-pd">
+              <h3>Get In Touch</h3>
+              <form>
+                <div style="margin-right: 1em">
+                  <input class="effect-1" type="text" placeholder="EMAIL">
+                  <span class="focus-border"></span>
+                </div>
+              </form>
+            </div>
+            <div class="col-md-4">
+              <h3>Follow Us</h3>
+              <ul class="social-line">
+                <li><a href="#"><i class="icon ion-logo-twitter"></i></a></li>
+                <li><a href="#"><i class="icon ion-logo-facebook"></i></a></li>
+                <li><a href="#"><i class="icon ion-logo-codepen"></i></a></li>
+                <li><a href="#"><i class="icon ion-logo-github"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- <div class="pd-3">
+
+    </div> -->
+
+<!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
 
     <section>
       <div class="footer">
@@ -123,7 +276,44 @@
     </section>
 
     <script type="text/javascript">
+      // JavaScript for label effects only
+      $(window).load(function(){
+        $("input").val("");
 
+        $(".input-effect input").focusout(function(){
+          if($(this).val() != ""){
+            $(this).addClass("has-content");
+          }else{
+            $(this).removeClass("has-content");
+          }
+        })
+      });
+
+      // TweenMax
+      $(function(){
+    	var $window = $(window);		//Window object
+
+    	var scrollTime = 1.2;			//Scroll time
+    	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+    	$window.on("mousewheel DOMMouseScroll", function(event){
+
+    		event.preventDefault();
+
+    		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+    		var scrollTop = $window.scrollTop();
+    		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+    		TweenMax.to($window, scrollTime, {
+    			scrollTo : { y: finalScroll, autoKill:true },
+    				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+    				autoKill: true,
+    				overwrite: 5
+    			});
+
+    	   });
+
+      });
     </script>
 
     <!-- <script src="https://unpkg.com/ionicons@4.2.5/dist/ionicons.js"></script> -->
